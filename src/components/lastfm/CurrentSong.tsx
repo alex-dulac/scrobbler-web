d05c3a05f5fcf2eab86b7122227ac999cf8394de
Alex Dulac <adulac29@gmail.com>
2024-08-24 11:09:13 -0400

    Recharts install and other updates

--- src/components/lastfm/CurrentSong.tsx
+++ src/components/lastfm/CurrentSong.tsx
@@ -1,17 +1,39 @@
-import React from "react";
-import {User} from "../models/user.model.ts";
+import React, {useEffect, useState} from "react";
 import {
   AppDispatch,
   RootState,
-} from "../store.ts";
+} from "../../store.ts";
 import {connect} from "react-redux";
-import {Song} from "../models/song.model.ts";
+import {Song} from "../../models/song.model.ts";
+import ScrobbleLineChart from "../data/ScrobbleLineChart.tsx";
+import {getCurrentSongScrobbles} from "../../api/service.ts";
 
 interface CurrentSongProps {
   currentSong: Song | null;
 }
 
 const CurrentSong: React.FC<CurrentSongProps> = ({currentSong}) => {
+  const [graphData, setGraphData] = useState(null);
+
+  useEffect(() => {
+    if (currentSong) {
+      getCurrentSongScrobbles().then(scrobbles => {
+        let graphData = [];
+
+        scrobbles.forEach(scrobble => {
+          graphData.push({
+            timestamp: scrobble.timestamp,
+            count: 1,
+          })
+        })
+
+        setGraphData(graphData);
+      });
+    } else {
+      setGraphData(null);
+    }
+  }, [currentSong?.id]);
+
   return (
     <div className={"current-song-widget"}>
       {!!currentSong && (
